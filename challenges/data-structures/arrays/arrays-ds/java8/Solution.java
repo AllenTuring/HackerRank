1edc3938ec84f44d902788d3c4370b70c115ea29
Allen Jiang <allen.jiang@outlook.com>
2017-07-21 17:59:53 -0400

    verifying size input

--- challenges/data-structures/arrays/arrays-ds/java8/Solution.java
+++ challenges/data-structures/arrays/arrays-ds/java8/Solution.java
@@ -5,11 +5,14 @@ public class Solution {
 
 	/*
 	Problem TLDR:
-	You get a number n, and then a list of numbers of size n.
+	You get a number n, and then a list of integers of size n.
 	Print the list in reverse.
 	*/
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        System.out.println(size);
     }
 }
