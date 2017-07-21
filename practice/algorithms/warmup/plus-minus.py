5b9a94be8e2cc2bf1bb5a7c9c0a43589f4d0fbcd
Allen Jiang <allen.jiang@outlook.com>
2017-07-21 16:45:44 -0400

    Solved Plus Minus as Python

--- practice/algorithms/warmup/plus-minus.py
+++ practice/algorithms/warmup/plus-minus.py
@@ -8,3 +8,27 @@ import sys
 
 n = int(input().strip())
 arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+# We could loop through and add to counters,
+# but I just want to use Python's list comprehensions here.
+# They're very neat.
+
+# Counters
+positive = len([e for e in arr if e > 0])
+negative = len([e for e in arr if e < 0])
+zero = len([e for e in arr if e == 0])
+
+print(positive/n)
+print(negative/n)
+print(zero/n)
+
+# A single loop would be faster (probably), and look like this.
+# for elem in arr:
+#     if elem > 0:
+#         positive += 1
+#     elif elem < 0:
+#         negative += 1
+#     else:
+#         zero += 1
+# ...
+# etc.
