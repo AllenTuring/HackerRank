c36e56fddcab7c7252876dbe34af863d2236ec26
Allen Jiang <allen.jiang@outlook.com>
2017-07-21 17:01:44 -0400

    Added Time Conversion as Python

---
+++ practice/algorithms/warmup/time-conversion.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import sys
+
+# Problem
+# You get a time string of format hh:mm:ssAM in 12 hour time.
+# Convert it to military time (24-hour clock) of form hh:mm:ss
+
+def timeConversion(s):
+    # Complete this function
+
+s = input().strip()
+result = timeConversion(s)
+print(result)
