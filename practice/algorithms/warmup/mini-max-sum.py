fe8c69ceeb7eeb92220114e100f4f275a295683d
Allen Jiang <allen.jiang@outlook.com>
2017-07-21 16:53:40 -0400

    Solved Mini-Max Sum as Python

--- practice/algorithms/warmup/mini-max-sum.py
+++ practice/algorithms/warmup/mini-max-sum.py
@@ -7,3 +7,12 @@ import sys
 # Print the sum of the smallest 4 and then the biggest 4.
 
 arr = list(map(int, input().strip().split(' ')))
+
+# First we add up all 5 numbers, no selecting.
+total = sum(arr)
+# Subtract the biggest number to get the sum of the smallest 4.
+min_sum = total - max(arr)
+# Subtract the smallest number to get the sum of the biggest 4.
+max_sum = total - min(arr)
+
+print(min_sum, max_sum)
