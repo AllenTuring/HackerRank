323b331fc942e6b49d84cf1c83c1dbc0c98e8e90
Allen Jiang <allen.jiang@outlook.com>
2017-07-21 18:15:38 -0400

    Solved Arrays - DS as Java 8

--- challenges/data-structures/arrays/arrays-ds/java8/Solution.java
+++ challenges/data-structures/arrays/arrays-ds/java8/Solution.java
@@ -12,7 +12,31 @@ public class Solution {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner in = new Scanner(System.in);
+        // Read the array size, then create an output array of that size.
         int size = in.nextInt();
-        System.out.println(size);
+        int[] output = new int[size];
+        // Write the array backwards to get the correct output.
+        for (int i = output.length - 1; i >= 0; i--) {
+            output[i] = in.nextInt();
+        }
+        // Although we could have built the string directly, we "should" use arrays here. Spirit of the problem.
+        System.out.println(intArrayToString(output));
+    }
+
+    /** Converts an int array into a space-separated string.
+    * @param input An array of integers
+    * @returns A String containing the ints in input, separated by spaces.
+    */
+    private static String intArrayToString(int[] input) {
+    	String output = "";
+        for (int i = 0; i < input.length; i++) {
+            // Add space before all but the first one
+            if (i > 0) {
+                output += " ";
+            }
+            // Add the next integer
+            output += input[i];
+        }
+        return output;
     }
 }
