f57c65919602b6af5ce1d80e6442040a67790032
Allen Jiang <allen.jiang@outlook.com>
2017-07-21 17:56:53 -0400

    added Arrays - DS as Java 8

---
+++ challenges/data-structures/arrays/arrays-ds/java8/Solution.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	/*
+	Problem TLDR:
+	You get a number n, and then a list of numbers of size n.
+	Print the list in reverse.
+	*/
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
