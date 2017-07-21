402cd4622285c455beb96d4e09471fb077932aae
Allen Jiang <allen.jiang@outlook.com>
2017-07-21 18:29:47 -0400

    added 2D Arrays - DS as Java 8

---
+++ challenges/data-structures/arrays/2d-array-ds/java8/Solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+/**
+* @author Allen Jiang
+* Solution class for HackerRank
+*/
+
+public class Solution {
+
+	/*
+	Problem TLDR:
+	You get a 6x6 2d array entered as string-separated numbers on separate lines.
+	The elements will be integers between -9 and 9 inclusive.
+	An hourglass is an I shape like this:
+	 1 2 3
+	   4
+	 5 6 7
+	Find the maximum hourglass sum, and print it.
+	*/
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
