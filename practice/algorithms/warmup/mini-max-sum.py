bc7f07871724ce711557738cabcab0d0a556f8ba
Allen Jiang <allen.jiang@outlook.com>
2017-07-21 16:51:52 -0400

    Added Mini-Max Sum as Python

---
+++ practice/algorithms/warmup/mini-max-sum.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+import sys
+
+# Problem
+# You get an array of 5 numbers.
+# Print the sum of the smallest 4 and then the biggest 4.
+
+arr = list(map(int, input().strip().split(' ')))
