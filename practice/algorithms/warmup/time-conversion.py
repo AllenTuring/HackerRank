60720e54ed75ae1bf6d3556599606aa72dfdb871
Allen Jiang <allen.jiang@outlook.com>
2017-07-21 17:05:54 -0400

    Solved Time Conversion as Python

--- practice/algorithms/warmup/time-conversion.py
+++ practice/algorithms/warmup/time-conversion.py
@@ -7,7 +7,29 @@ import sys
 # Convert it to military time (24-hour clock) of form hh:mm:ss
 
 def timeConversion(s):
-    # Complete this function
+    # Break up the time string into blocks
+    # This is Python string slicing.
+    hh = s[:2]
+    mmss = s[2:-2]
+    block = s[-2:]
+    
+    # Correct the 12: to 00: when going between the clocks
+    # Very tricky nuance: 12:-- PM is actually better regarded as 00:-- PM
+    # Military time uses the 00 and 12, 12 AM and 12 PM respectively.
+    if hh == "12":
+        hh = "00"
+    
+    # If it's PM, we just take the time and add 12.
+    # Assuming a valid input, we shouldn't need to % 24.
+    if block == "PM":
+        # add 12 hours if PM
+        hh = str((int(hh) + 12))
+        # fill in any lost zero padding so it's still 2 digits.
+        if len(hh) == 1:
+            hh = "0" + hh
+            
+    # Return the corrected time. Note: no AM/PM in 24 hour clock.
+    return hh + mmss
 
 s = input().strip()
 result = timeConversion(s)
