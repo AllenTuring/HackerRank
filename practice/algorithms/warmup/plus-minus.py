b18ffbc7192028df43c55de03731027b99b6c323
Allen Jiang <allen.jiang@outlook.com>
2017-07-21 16:40:45 -0400

    Added Plus Minus as Python

---
+++ practice/algorithms/warmup/plus-minus.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+import sys
+
+# Problem:
+# You get an array arr of size n.
+# Print the fractions of numbers in arr that are positive, negative, and zero
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
