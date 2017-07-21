f29a2f17e27fcc52cc6b2061382e50880fdbc1ea
Allen Jiang <allen.jiang@outlook.com>
2017-07-21 16:59:16 -0400

    Solved Birthday cake Candles as Python

--- practice/algorithms/warmup/birthday-cake-candles.py
+++ practice/algorithms/warmup/birthday-cake-candles.py
@@ -7,7 +7,17 @@ import sys
 # You blow out the tallest candles only. How many do you blow?
 
 def birthdayCakeCandles(n, ar):
-    # Complete this function
+    # First we getses the height of the tallest preciousss
+    high = max(ar)
+
+    # And then we sees how many preciousesss are that tall
+    blown = 0
+    for candle in ar:
+        if candle == high:
+        	# We blow all the tallest precioussses
+            blown += 1
+    
+    return blown
 
 n = int(input().strip())
 ar = list(map(int, input().strip().split(' ')))
