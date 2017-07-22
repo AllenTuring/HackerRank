13cdec3d67085a3e053bcb016e1e061792b3533d
Allen Jiang <allen.jiang@outlook.com>
2017-07-21 20:26:35 -0400

    added Dynamic Array as Java 8

---
+++ challenges/data-structures/arrays/dynamic-array/java8/Solution.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+/**
+* @author Allen Jiang
+* Solution class for HackerRank
+*/
+
+public class Solution {
+
+	/*
+	Problem:
+	You must keep track of an int, here called lastAnswer, and a list of empty sequences.
+	You get 2 numbers to begin with:
+	- A number of sequences (n) to keep track of
+	- A number of commands that follow.
+	Then is a list of commands of format: c x y
+	If c is 1, then:
+	- Find sequence # (x xor lastanswer) % n,
+	- Append y to that sequence.
+	If c is 2, then:
+	- Find sequence # (x xor lastanswer) % n,
+	- Find element # y (modulo with size of sequence to wrap)
+	- Write that element to lastAnswer, and then print it.
+	*/
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
