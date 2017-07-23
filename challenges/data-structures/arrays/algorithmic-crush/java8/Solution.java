1ab8474aaaf91226a813e91d12f685df96076329
Allen Jiang <allen.jiang@outlook.com>
2017-07-23 18:56:35 -0400

    added Algorithmic Crush as Java 8

---
+++ challenges/data-structures/arrays/algorithmic-crush/java8/Solution.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+/**
+* @author Allen Jiang
+* Solution class for HackerRank
+*/
+
+public class Solution {
+
+	 public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
