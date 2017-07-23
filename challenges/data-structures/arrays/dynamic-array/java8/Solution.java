1fdcd2f53b7ff5723bc5e0a9b4e79a49d914768e
Allen Jiang <allen.jiang@outlook.com>
2017-07-23 15:36:24 -0400

    dynamic-array: double-checked logic, even side-by-side comparison and handwork. Issue must be runtime resource usage, swapping to ArrayList<ArrayList<Integer>> to test.

--- challenges/data-structures/arrays/dynamic-array/java8/Solution.java
+++ challenges/data-structures/arrays/dynamic-array/java8/Solution.java
@@ -4,25 +4,10 @@ import java.util.*;
 
 public class Solution {
 
-
-	/*
-	Problem:
-	You must keep track of an int, here called lastAnswer, and a list of empty sequences.
-	You get 2 numbers to begin with:
-	- A number of sequences (n) to keep track of, which is also the max length of each.
-	- A number of commands that follow.
-	Then is a list of commands of format: c x y
-	If c is 1, then:
-	- Find sequence # (x xor lastanswer) % n,
-	- Append y to that sequence.
-	If c is 2, then:
-	- Find sequence # (x xor lastanswer) % n,
-	- Find element # y (modulo with size of sequence to wrap)
-	- Write that element to lastAnswer, and then print it.
-	*/
-
 	// List of sequences to keep track of, and their array stored sizes
-	private static FastIntArrayList[] seqList;
+	private static ArrayList<ArrayList<Integer>> seqList;
+	// Storage for n
+	private static int n;
 	// The lastAnswer storage
 	private static int lastAnswer;
 	// Read input scanner from system.in
@@ -30,13 +15,13 @@ public class Solution {
 
     public static void main(String[] args) {
     	// Read the first two inputs.
-    	int size = in.nextInt();
+    	n = in.nextInt();
     	int commandCount = in.nextInt();
 
     	// Initialize seqList to the correct size.
-    	seqList = new FastIntArrayList[size];
-        for (int i = 0; i < size; i++) {
-            seqList[i] = new FastIntArrayList(size);
+    	seqList = new ArrayList<ArrayList<Integer>>(n);
+        for (int i = 0; i < n; i++) {
+            seqList.add(new ArrayList<Integer>(n));
         }
 
     	// Read the specified number of commands.
@@ -52,7 +37,7 @@ public class Solution {
     	int command = in.nextInt();
     	int x = in.nextInt();
     	int y = in.nextInt();
-    	int index = x ^ lastAnswer;
+    	int index = (x ^ lastAnswer) % n;
     	// Execute them.
     	switch (command) {
     		case 1:
@@ -70,10 +55,7 @@ public class Solution {
 	* @param val The value to append
     */
     private static void append(int index, int val) {
-    	// Modulo wrap the index value
-    	index = index % seqList.length;
-    	// Find the subindex value of the tail.
-    	seqList[index].add(val);
+    	seqList.get(index).add(val);
     }
 
     /** Reads the value at the specified index and subindex in the sequence list
@@ -82,39 +64,7 @@ public class Solution {
 	* @return The element at the specified indices in the sequence list
     */
     private static int read(int index, int subindex) {
-    	index = index % seqList.length;
-    	return seqList[index].get(subindex);
+        ArrayList<Integer> seq = seqList.get(index);
+    	return seq.get(subindex % seq.size());
     }
-    
-    /** A class for wrapping the data as a non-generic int array list.
-    * Note that this data structure does not expand.
-    */
-    private static class FastIntArrayList {
-        int[] data;
-        int size;
-        
-        /** Constructor for a new FastIntArrayList
-        * @param maxSize the maximum size of this array list.
-        */
-        public FastIntArrayList(int maxSize) {
-            this.size = 0;
-            this.data = new int[maxSize];
-        }
-        
-        /** Adds a new element to the end of this array list.
-        * @param elem The element to add.
-        */
-        public void add(int elem) {
-            data[size] = elem;
-            size++;
-        }
-        
-        /** Gets the specified element of this array list
-        * @param index The index of the element to return.
-        */
-        public int get(int index) {
-            return data[index % size];
-        }
-    }
-
 }
