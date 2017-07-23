9118ce1501271f45f54a23972f64f8c2b2bddb94
Allen Jiang <allen.jiang@outlook.com>
2017-07-23 18:24:03 -0400

    added Sparse Arrays as Java 8

---
+++ challenges/data-structures/arrays/sparse-arrays/java8/Solution.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+/**
+* @author Allen Jiang
+* Solution class for HackerRank
+*/
+
+public class Solution {
+
+	/*
+	Problem:
+	You are given a number N, followed by N strings on separate lines. hint: use in.nextLine()
+	Then a number Q, followed by Q queries on separate lines.
+	For each query, find the number of occurrences in the strings.
+	*/
+
+	public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
