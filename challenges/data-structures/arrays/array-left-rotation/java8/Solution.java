3c96beec1bb0fe4229fd58b476c15eb874c6f1ea
Allen Jiang <allen.jiang@outlook.com>
2017-07-23 16:07:24 -0400

    array-left-rotation: wrote code framework, stub for rotate

--- challenges/data-structures/arrays/array-left-rotation/java8/Solution.java
+++ challenges/data-structures/arrays/array-left-rotation/java8/Solution.java
@@ -16,7 +16,58 @@ public class Solution {
 	[1 2 3 4] rotate 1 -> [4 1 2 3]
 	*/
 
+	// Read input scanner from system.in
+	private static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        // Read the array size and distance to rotate
+        int len = in.nextInt();
+        int dist = in.nextInt();
+        // Read the array
+        int[] data = readArray(len);
+
+        data = rotate(data, dist);
+
+        // Output
+        System.out.println(intArrayToString(input));
+    }
+
+    /** Returns a new, rotated array based on an old array
+	* @param data The input array
+    * @param dist The distance to rotate
+    * @returns The array of integers read.
+    */
+    private static int[] rotateLeft(int[] data, int dist) {
+    	// code here
+    	return null;
+    }
+
+    /** Reads an int array from stdin.
+    * @param len The length of the array
+    * @returns The array of integers read.
+    */
+    private static int[] readArray(int len) {
+    	int[] data = new int[len];
+    	for (int i = 0; i < len; i++) {
+    		data[i] = in.nextInt();
+    	}
+    	return data;
+    }
+
+    /** Converts an int array into a space-separated string.
+    * @param input An array of integers
+    * @returns A String containing the ints in input, separated by spaces.
+    */
+    private static String intArrayToString(int[] input) {
+    	String output = "";
+        for (int i = 0; i < input.length; i++) {
+            // Add space before all but the first one
+            if (i > 0) {
+                output += " ";
+            }
+            // Add the next integer
+            output += input[i];
+        }
+        return output;
     }
 }
