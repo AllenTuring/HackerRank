5477550985faeee04453f5ce4200fdab04f6cf46
Allen Jiang <allen.jiang@outlook.com>
2017-07-23 16:02:42 -0400

    added Array Left Rotation as Java 8

---
+++ challenges/data-structures/arrays/array-left-rotation/java8/Solution.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+/**
+* @author Allen Jiang
+* Solution class for HackerRank
+*/
+
+public class Solution {
+
+	/*
+	Problem:
+	You are given 2 numbers: n and d
+	Then you are given an array of numbers of length n, into stdin.
+	Rotate them by d to the left and then print them. Example:
+	[1 2 3 4] rotate 1 -> [4 1 2 3]
+	*/
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
