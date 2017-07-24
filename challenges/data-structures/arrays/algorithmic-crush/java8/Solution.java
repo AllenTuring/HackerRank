fc8166602203370b6ca8e1ef80ac921da2b323de
Allen Jiang <allen.jiang@outlook.com>
2017-07-23 20:48:24 -0400

    partial solutions for Algorithmic Crush, saved java8/naive and a Partitioned solution

--- challenges/data-structures/arrays/algorithmic-crush/java8/Solution.java
+++ challenges/data-structures/arrays/algorithmic-crush/java8/Solution.java
@@ -19,6 +19,7 @@ public class Solution {
 
 	// Read input scanner from system.in
 	private static Scanner in = new Scanner(System.in);
+	private static PartitionedSumList list;
 
 	public static void main(String[] args) {
         // Initialize the list
@@ -28,7 +29,8 @@ public class Solution {
         // Read commands
         int commandCount = in.nextInt();
         for (int i = 0; i < commandCount; i++) {
-        	add(in.nextInt(), in.nextInt(), in.nextInt());
+        	// Read in the command, swapping to zero indexed, inclusive-exclusive.
+        	add(in.nextInt() - 1, in.nextInt(), in.nextInt());
         }
 
         System.out.println(getMax());
@@ -38,7 +40,7 @@ public class Solution {
     * @param size The size of the list to initialize
     */
     private static void initList(int size) {
-    	// TODO stub
+    	list = new PartitionedSumList(size);
     }
 
     /** Adds a value onto a specified range of the list
@@ -47,7 +49,7 @@ public class Solution {
     * @param b The last index to be processed
     */
     private static void add(int a, int b, int val) {
-    	// TODO stub
+    	list.add(a, b, val);
     }
 
     /** Finds the maximum value in the list
@@ -55,7 +57,101 @@ public class Solution {
     */
     private static int getMax() {
     	// TODO stub
-    	return 0;
+    	return list.getMax();
     }
 
+    /** Class to govern the list tracking, reduce redundancy */
+    private static class PartitionedSumList {
+    	// A list of values in sequential read order
+    	private List<PartitionedSum> data;
+
+    	/** Constructor to make a new PartitionedSumList.
+    	* @param size The size of the imaginary list
+    	*/
+    	public PartitionedSumList(int size) {
+    		this.data = new ArrayList<PartitionedSum>();
+    		this.data.add(new PartitionedSum(0, size, 0));
+    	}
+
+    	/** Adds a value onto a specified range of the list
+	    * @param val The value to be added
+	    * @param a The first index to be processed
+	    * @param b The last index to be processed
+	    */
+	    private void add(int a, int b, int val) {
+    		int dataSize = data.size();
+    		PartitionedSum datum;
+    		// Loop through the data points backwards for inflation
+	    	for (int i = dataSize - 1; i >= 0; i--) {
+	    		datum = data.get(i); 
+	    		// Ask each data node how the range [a, b) affects it
+	    		if (datum.intersects(a, b)) {
+	    			// If so we need to take action
+	    			// Buffer off the first half if necessary
+	    			if (datum.a < a) {
+	    				// split into two along a
+	    				PartitionedSum headSplit = new PartitionedSum(datum.a, a, datum.value);
+	    				datum.a = a;
+	    				// insert the new half in the data registry
+	    				data.add(headSplit);
+	    			}
+	    			// Then the second half
+	    			if (b < datum.b) {
+	    				// split into two along b
+	    				PartitionedSum tailSplit = new PartitionedSum(b, datum.b, datum.value);
+	    				datum.b = b;
+	    				// insert the new half in the data registry
+	    				data.add(tailSplit);
+	    			}
+	    			// Finally add val to what's left in the middle.
+		    		datum.value += val;
+	    		}
+	    		// If it doesn't apply, just skip (no code necessary)
+	    	}
+	    }
+
+		/** Finds the maximum value of all partitioned sums
+	    * @returns The largest partitioned sum
+	    */
+    	public int getMax() {
+    		int max = data.get(0).value;
+    		int dataSize = data.size();
+    		// Loop through the data
+    		for (int i = 0; i < dataSize; i++) {
+    			// Get values, if they are bigger than max then set max to that
+    			int val = data.get(i).value;
+    			if (val > max) {
+    				max = val;
+    			}
+    		}
+    		return max;
+    	}
+
+    	private static class PartitionedSum {
+    		// The value within this partition segment
+    		public int value;
+    		// The starting index of this segment (inclusive)
+    		public int a;
+    		// The ending index of this segment (inclusive)
+    		public int b;
+
+    		/** Constructor to make a new PartitionedSum
+	    	* @param size The value inside this segment
+	    	* @param a The starting index of this segment (inclusive)
+	    	* @param b The ending index of this segment (exclusive)
+	    	*/
+	    	public PartitionedSum(int a, int b, int val) {
+	    		this.value = val;
+	    		this.a = a;
+	    		this.b = b;
+	    	}
+
+	    	/** Returns whether or not the range [a, b) add operation applies to this partition's range
+	    	* @return True if and only if the current node has interval overlap
+	    	*/
+	    	public boolean intersects(int a, int b) {
+	    		return this.a < b && a < this.b;
+	    	}
+    	}
+    }
 }
