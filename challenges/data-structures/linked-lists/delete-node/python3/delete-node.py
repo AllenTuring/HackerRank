91624ee3960417f5bcc113689d36b580d27e578c
Allen Jiang <allen.jiang@outlook.com>
2017-08-06 18:19:08 -0400

    Solved Delete Node as Python 3

---
+++ challenges/data-structures/linked-lists/delete-node/python3/delete-node.py
@@ -0,0 +1,20 @@
+"""
+ Delete Node at a given position in a linked list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+ return back the head of the linked list in the below method. 
+"""
+# Author: Allen Jiang
+# Source: HackerRank
+def Delete(head, position):
+  
+  
+  
+  
+  
